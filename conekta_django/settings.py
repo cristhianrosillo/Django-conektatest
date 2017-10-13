14defd25f9d3968d955d84616d9ef3978d8d3c6c
cristhianrosillo <rosillocristhian@gmail.com>
2017-10-13 18:50:44 -0500

    1

--- conekta_django/settings.py
+++ conekta_django/settings.py
@@ -127,4 +127,4 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorag'
