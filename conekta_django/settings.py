d374e1dbdea8c7305f1f0815a74f48297d8290cd
cristhianrosillo <rosillocristhian@gmail.com>
2017-10-15 02:37:47 -0500

    1

--- conekta_django/settings.py
+++ conekta_django/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     'django.contrib.sites',
     'sales.apps.SalesConfig',
     'conekta'
