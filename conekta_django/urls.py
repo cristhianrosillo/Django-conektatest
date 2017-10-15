4e92f4bfc83ad823cab968220093d5c44df7fc87
cristhianrosillo <rosillocristhian@gmail.com>
2017-10-15 02:30:16 -0500

    1

--- conekta_django/urls.py
+++ conekta_django/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
